21bd3034c1ed62b345f00eed0577e5d623c94103
neoxv <yaredoo2k4@gmail.com>
2022-05-10 17:18:10 +0300

    added reset button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,11 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
                 </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                <x-button  class="bg-red-500" onclick="{{route('')}}">
+                    {{ __('Reset Password') }}
+                </x-button>
+                </div>
             </div>
         </div>
     </div>

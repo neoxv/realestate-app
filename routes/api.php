17c097056bfb933e68d5ab06d5f297a45007187c
neoxv <yaredoo2k4@gmail.com>
2022-10-20 17:47:41 +0300

    added header inside api.php

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,5 @@
 <?php
 
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
-
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    $request->header("Cache-Control: no-cache, must-revalidate");
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+    header('Access-Control-Allow-Origin:  *');
+    header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+    header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
     return $request->user();
 });
